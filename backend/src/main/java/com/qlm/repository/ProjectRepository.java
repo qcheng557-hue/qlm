6fd791cae06d49d43cdede08ad992cc4ce18a768
qcheng557-hue <qcheng557@gmail.com>
2026-06-03 03:19:51 +0800

    添加 ProjectRepository

---
+++ backend/src/main/java/com/qlm/repository/ProjectRepository.java
@@ -0,0 +1,12 @@
+package com.qlm.repository;
+
+import com.qlm.entity.Project;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends JpaRepository<Project, Long> {
+    Page<Project> findByNameContainingIgnoreCase(String name, Pageable pageable);
+}
