fedd522d3c2010c947d7fc9493d594ab889933de
qcheng557-hue <qcheng557@gmail.com>
2026-06-02 19:25:05 +0800

    添加后端主应用入口类

---
+++ backend/src/main/java/com/qlm/QlmApplication.java
@@ -0,0 +1,11 @@
+package com.qlm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QlmApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(QlmApplication.class, args);
+    }
+}
